a3df646756041cf4917c394fe180ec7bbdf43124
horaciocoronel <arielc@gmail.com>
2017-11-20 16:52:16 -0500

    Add seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,22 @@
 
 User.create(username: 'michelle', email: 'michelle@gmail.com', password: '123',
     password_confirmation: '123')
+User.create(username: 'horacio', email: 'horacio@gmail.com', password: '123',
+        password_confirmation: '123')
+User.create(username: 'ari', email: 'ari@gmail.com', password: '123',
+        password_confirmation: '123')
+
+10.times do
+  following = Following.create!(
+              user_id: rand(1..3),
+              coin_name: ['BTC','ETH', 'BCH', 'XRP', 'LTC'].sample
+            )
+end
+20.times do
+  alerts = Alert.create!(
+              price_above: rand(10533.7395700..10533.7395762),
+              price_below: rand(10533.7395600..10533.7395700),
+              state: 'Active',
+              following_id: rand(1..10)
+            )
+end
