8228886aaf49a555c7a07296c014baa84d8c097c
Michelle Cinello <m.cinello@gmail.com>
2017-11-20 17:58:20 -0500

    set up following index and controller

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,19 +1,20 @@
 class FollowingController < ApplicationController
-#   def index
-#     # if user_id == current_user
-#     #@following = Following.where(user_id: current_user)
-#   end
-#
-#   def new
-#     @following = Following.new
-#   end
-#
-#   def create
-#     @following = Following.new
-#     @following.user_id = current_user
-#     @following.coin_name = params[:coin]["symbol"]
-#
-#     if @following.save
-#       redirect_to
-#   end
-# end
+  def index
+    @following = Following.where(user_id: 1)
+  end
+
+  def new
+    @following = Following.new
+  end
+
+  def create
+    call_coin_market_cap_api
+    @following = Following.new
+    @following.user_id = current_user
+    @following.coin_name = params[:coin]["symbol"]
+
+    # if @following.save
+    #   redirect_to
+    # end
+  end
+end
