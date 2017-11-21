9e5c290e5ac9c61cc2cc9cb60e4786867c9afe56
horaciocoronel <arielc@gmail.com>
2017-11-21 17:39:26 -0500

    Add AMCharts to coin show view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,61 @@
 //= require turbolinks
 //= require materialize
 //= require_tree .
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+var info = document.getElementById('chartdiv');
+var chart = AmCharts.makeChart( "chartdiv", {
+  "type": "serial",
+  "theme": "light",
+  "dataDateFormat":"YYYY-MM-DD hh:mm:ss",
+  "valueAxes": [ {
+    "position": "left"
+  } ],
+  "graphs": [ {
+    "id": "g1",
+    "proCandlesticks": true,
+    "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
+    "closeField": "close",
+    "fillColors": "#7f8da9",
+    "highField": "high",
+    "lineColor": "#7f8da9",
+    "lineAlpha": 1,
+    "lowField": "low",
+    "fillAlphas": 0.9,
+    "negativeFillColors": "#db4c3c",
+    "negativeLineColor": "#db4c3c",
+    "openField": "open",
+    "title": "Price:",
+    "type": "candlestick",
+    "valueField": "close"
+  } ],
+  "chartScrollbar": {
+    "graph": "g1",
+    "graphType": "line",
+    "scrollbarHeight": 30
+  },
+  "chartCursor": {
+    "valueLineEnabled": true,
+    "valueLineBalloonEnabled": true
+  },
+  "categoryField": "date",
+  "categoryAxis": {
+    "parseDates": true
+  },
+  "dataProvider": JSON.parse(info.dataset.chartarray),
+  "export": {
+    "enabled": true,
+    "position": "bottom-right"
+  }
+} );
+
+chart.addListener( "rendered", zoomChart );
+zoomChart();
+
+// this method is called when chart is first inited as we listen for "dataUpdated" event
+function zoomChart() {
+  // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+  chart.zoomToIndexes( chart.dataProvider.length - 50, chart.dataProvider.length - 1 );
+}
+  });
--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -36,6 +36,12 @@ class CoinsController < ApplicationController
     @data_by_minute = HTTParty.get('https://min-api.cryptocompare.com/data/histominute?fsym=ETH&limit=2000&tsym=BTC&aggregate=3&e=Bittrex&allData=true')
     @data_by_minute_result = JSON.parse(@data_by_minute.body)
     @data_by_minute_result = @data_by_minute_result["Data"]
+
+# {"time"=>1510660800, "close"=>0.0499, "high"=>0.05, "low"=>0.04969, "open"=>0.04991, "volumefrom"=>219.73999999999998, "volumeto"=>10.97}
+    @data_by_minute_result.each do |element|
+      element["date"] = Time.at(element["time"]).to_s
+    end
+
   end
 
 end
