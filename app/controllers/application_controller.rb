cdc22f9f85ca156efe3f0f118ef250db30a5cdd2
horaciocoronel <arielc@gmail.com>
2017-11-20 20:10:31 -0500

    Add styling to Coins page, add table listing top 5 coins, call CryptoCompare API for images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def call_cryptocompare_api
+    @cryptocompare = HTTParty.get('https://www.cryptocompare.com/api/data/coinlist/')
+    @cryptocompare_result = JSON.parse(@cryptocompare.body)
+    @cryptocompare_result = @cryptocompare_result["Data"]
+    base_cc_url = @cryptocompare_result["BaseImageUrl"]
+    # coin_image = @cryptocompare_result[cc_symbol]["ImageUrl"]
+    # @coin_image_url = "#{base_cc_url}+#{coin_image}"
+  end
 end
--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -2,6 +2,8 @@ class CoinsController < ApplicationController
   def index
     @response = HTTParty.get('https://api.coinmarketcap.com/v1/ticker/?convert=CAD&limit=5')
     @result = JSON.parse(@response.body)
+
+    call_cryptocompare_api
   end
 
   def show
