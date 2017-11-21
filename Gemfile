85f9e92b4398ad8c11d172fb3569dd6051c6d277
Argir Argirov <dj.giro@gmail.com>
2017-11-21 15:18:01 -0500

    sorcery gem added, resource for sessions, user login, validations added

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'materialize-rails', '~> 0.97.5.2'
 gem 'material_icons'
 
+gem 'sorcery'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :ensure_logged_in
+  helper_method :ensure_ownership
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # def ensure_logged_in
+  #   if !current_user
+  #     flash[:alert] = "Please log in"
+  #     redirect_to root_url
+  #   end
+  # end
+  #
+  # def ensure_ownership
+  #   if current_user.id != @project.user_id
+  #     flash[:alert] = 'Not authorized owner'
+  #     redirect_to root_url
+  #   end
+  # end
+
 end
