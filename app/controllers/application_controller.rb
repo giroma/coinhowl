71402965392cade292abcba16321576399bc0578
horaciocoronel <arielc@gmail.com>
2017-11-24 12:31:04 -0500

    Add remaining images by remapping the name and url from cryptocompare

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def call_cryptocompare_api
-    @cryptocompare = HTTParty.get('https://www.cryptocompare.com/api/data/coinlist/')
+    @cryptocompare = HTTParty.get('https://www.cryptocompare.com/api/data/coinlist')
     @cryptocompare_result = JSON.parse(@cryptocompare.body)
     @cryptocompare_result = @cryptocompare_result["Data"]
     base_cc_url = @cryptocompare_result["BaseImageUrl"]
