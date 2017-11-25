2d7dcd1b316af4b60618720a8a4c78039d40b7fb
horaciocoronel <arielc@gmail.com>
2017-11-25 15:59:25 -0500

    Add alert popup on alert page

---
+++ app/assets/javascripts/alerts.js
@@ -0,0 +1,13 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+  $(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
+
+
+});
