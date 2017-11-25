77146665eac3b6489f09d794c3bdd726170080c0
Michelle Cinello <m.cinello@gmail.com>
2017-11-25 15:48:19 -0500

    added comments to clarify the table in the coins show page. changed Alert.create to Alert.new,. Deleted the root_url in show page, created alerts.js file

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -6,6 +6,8 @@ class CoinsController < ApplicationController
 
   def show
     @coin_symbol = params[:id]
+
+    # This confirms the follow/unfollow display of current user
     @follow = Following.where(user_id: current_user.id, coin_name: @coin_symbol)
     @is_following = @follow.length > 0 ? true : false
     get_chart_data_by_minute
