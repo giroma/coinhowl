c21daa555635aba92575f4148580d03a1a814ef4
horaciocoronel <arielc@gmail.com>
2017-11-27 13:10:36 -0500

    Add columns to user model  alert by phone and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127064830) do
+ActiveRecord::Schema.define(version: 20171127180454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20171127064830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone"
+    t.boolean "phone_alert"
+    t.boolean "email_alert"
   end
 
   add_foreign_key "alerts", "followings"
