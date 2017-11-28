ad1bbf6b66152cd0de5179563e0e306255cc2c1b
horaciocoronel <arielc@gmail.com>
2017-11-28 10:07:38 -0500

    Integration of new materialize-sass gem, migration of existin functions to new gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,40 +20,44 @@
 //= stub coins
 
 $(document).ready(function() {
-var trig = 1;
-//fix for chrome
-$("#search").addClass('searchbarfix');
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  // $('.carousel-slider').slider({full_width: true});//slider init
 
 
-  //animate searchbar width increase to  +150%
-  $('#search').click(function(e) {
-       //handle other nav elements visibility here to avoid push down
-    $('.search-hide').addClass('hide');
-   if (trig == 1){
-      $('#navfix2').animate({
-        width: '+=150',
-        marginRight: 0
-      }, 400);
-
-     trig ++;
-     }
-
-  });
-
-  // if user leaves the form the width will go back to original state
-
-  $("#search").focusout(function() {
-
-      $('#navfix2').animate({
-      width: '-=150'
-    }, 400);
-   trig = trig - 1;
-    //handle other nav elements visibility first to avoid push down
-    $('.search-hide').removeClass('hide');
-
-  });
-
-  var symbolArray = []
+// var trig = 1;
+// //fix for chrome
+// $("#search").addClass('searchbarfix');
+//
+//
+//   //animate searchbar width increase to  +150%
+//   $('#search').click(function(e) {
+//        //handle other nav elements visibility here to avoid push down
+//     $('.search-hide').addClass('hide');
+//    if (trig == 1){
+//       $('#navfix2').animate({
+//         width: '+=150',
+//         marginRight: 0
+//       }, 400);
+//
+//      trig ++;
+//      }
+//
+//   });
+//
+//   // if user leaves the form the width will go back to original state
+//
+//   $("#search").focusout(function() {
+//
+//       $('#navfix2').animate({
+//       width: '-=150'
+//     }, 400);
+//    trig = trig - 1;
+//     //handle other nav elements visibility first to avoid push down
+//     $('.search-hide').removeClass('hide');
+//
+//   });
+var myResults = function() {
+  var symbolHash = {}
   var url = "https://cors-anywhere.herokuapp.com/https://bittrex.com/api/v1.1/public/getmarketsummaries";
   $.ajax({
   url: url,
@@ -61,25 +65,42 @@ $("#search").addClass('searchbarfix');
   dataType: 'JSON'
   }).done(function(data){
   data["result"].forEach(function(element) {
-    // var tempSymbol = element["MarketName"];
     var tempSymbol = element["MarketName"].includes('BTC-','');
     if(tempSymbol == true) {
       var btcRemoved = element["MarketName"].replace('BTC-','');
-      symbolArray.push(btcRemoved);
+      symbolHash[btcRemoved] = null
     }
 });
-  return symbolArray
+  return symbolHash;
 });
+console.log(symbolHash);
 
+};
 
-  $(function() {
-    $('input.autocomplete').autocomplete({
-      select: function( event, ui ) {
-        var url = ui.item.label;
-        window.location = '/coins/'+url
-      },
-      source: symbolArray
-    });
+// console.log(dataForIs);
+  $('input.autocomplete').autocomplete({
+   data: myResults(),
+   limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+   onAutocomplete: function(val) {
+     window.console&&console.log(dataForIs);
+     // Callback function when value is autcompleted.
+         // select: function( event, ui ) {
+         //   var url = ui.item.label;
+         //   window.location = '/coins/'+url
+         // },
+   },
+   minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
   });
+
+
+  // $(function() {
+  //   $('input.autocomplete').autocomplete({
+  //     select: function( event, ui ) {
+  //       var url = ui.item.label;
+  //       window.location = '/coins/'+url
+  //     },
+  //     source: symbolArray
+  //   });
+  // });
 
 });
