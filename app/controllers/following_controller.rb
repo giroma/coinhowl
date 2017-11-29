7803b27ad1288b81fe4cd1231fe24da7c04101d5
horaciocoronel <arielc@gmail.com>
2017-11-29 11:28:11 -0500

    Fix error in follow/unfollow button when clicking on it too fast

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,6 +1,6 @@
 class FollowingController < ApplicationController
   before_action :call_bittrex
-    
+
   def index
     @user = current_user
     @following = Following.where(user_id: @user.id)
@@ -33,7 +33,9 @@ class FollowingController < ApplicationController
   def destroy
     @coin_symbol = params[:id]
     @follow = Following.find_by(user_id: current_user.id, coin_name: @coin_symbol)
-    @follow.alerts.delete_all
+    if @follow != nil
+      @follow.alerts.delete_all
+    end
     respond_to do |format|
       format.html
       format.json do
