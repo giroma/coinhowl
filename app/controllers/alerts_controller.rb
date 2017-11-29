6cc20a8999adc19c31dd6c4fd4da060616d5d777
Michelle Cinello <m.cinello@gmail.com>
2017-11-29 08:32:54 -0500

    changed the render to redirect_to under the else of @alert.save

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -11,7 +11,7 @@ class AlertsController < ApplicationController
     if @alert.save
       redirect_to following_index_path
     else
-      render coin_path(@coin)
+      redirect_to coin_path(@coin)
     end
     # respond_to do |format|
     #   format.html
