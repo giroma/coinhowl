91965656bce2045139aa09ab51e45a1ecddf1756
Michelle Cinello <m.cinello@gmail.com>
2017-12-01 06:17:22 -0500

    validated the page redirection and alert if a user is not logged in and trying to follow the account, as well as setting up the path and error alert if a current user is clicking the follow button too many times

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -9,27 +9,32 @@ class FollowingController < ApplicationController
   end
 
   def update
-    @coin_symbol = params[:id]
-    @is_following = Following.find_by(user_id: current_user.id, coin_name: @coin_symbol)
-    if current_user && !@is_following
-    @follow = Following.new
-    @follow.user_id = current_user.id
-    @follow.coin_name = params[:id]
+      if current_user
+        @coin_symbol = params[:id]
+        @is_following = Following.find_by(user_id: current_user.id, coin_name: @coin_symbol)
+        if !@is_following
+          @follow = Following.new
+          @follow.user_id = current_user.id
+          @follow.coin_name = params[:id]
 
-    respond_to do |format|
-      format.html
-      format.json do
-        if @follow.save
-          render json:{}, nothing: true, status: 204
+          respond_to do |format|
+            format.html
+            format.json do
+              if @follow.save
+                render json:{}, nothing: true, status: 204
+              else
+                render json:{}, nothing: true, status: 400
+              end
+            end
+          end
         else
-          render json:{}, nothing: true, status: 400
+          flash[:alert] = "Sorry, something went wrong. Please try again."
+          redirect_to coin_path
         end
-      end
-    end
-    else
-      # flash[:alert] = "You must sign in to follow a coin."
-      redirect_to coin_path
-    end
+      else
+          flash[:alert] = "You must have an account to follow a coin."
+          redirect_to new_user_path
+        end
   end
 
   def destroy
