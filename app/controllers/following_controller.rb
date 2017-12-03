9dd8eeaedc2bee27dd7341ef961d37ad2d438927
Michelle Cinello <m.cinello@gmail.com>
2017-12-03 08:37:36 -0500

    fixed the destroy error with alerts when a coin is unfollowed. used wrong method delete_all, must use destroy_all

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -45,9 +45,7 @@ class FollowingController < ApplicationController
   def destroy
     @coin_symbol = params[:id]
     @follow = Following.find_by(user_id: current_user.id, coin_name: @coin_symbol)
-    if @follow != nil
-      @follow.alerts.delete_all
-    end
+    @follow.alerts.destroy_all
     respond_to do |format|
       format.html
       format.json do
