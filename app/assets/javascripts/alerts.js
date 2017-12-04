a380aa4444c6021c4ef3010df5aafd73bc7dcac7
horaciocoronel <arielc@gmail.com>
2017-12-04 11:10:18 -0500

    Fix counter error when deleting alerts"

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -6,11 +6,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   // $('.modal-trigger').leanModal();
    $('.modal').modal();
-
-  var alertCount = document.querySelector('.alert-count');
-  var alertWolf = document.querySelector('#wolf-alert');
+  var activeli = document.querySelector('.active');
+  // var alertWolf = document.querySelector('#wolf-alert');
 
   $('.delete_alert').bind('ajax:success', function() {
+    var alertCount = $(this).closest('.active')[0].childNodes[1].childNodes[4].nextSibling.childNodes[1].childNodes[2];
+    var alertWolf = $(this).closest('.active')[0].childNodes[1].childNodes[4].nextSibling.childNodes[1]
           $(this).closest('tr').fadeOut();
           alertCount.innerText -= 1;
           if (alertCount.innerText === "0") {
