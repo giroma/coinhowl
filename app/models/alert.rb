770e5787d70822e1389af43ee2c26c124ea2ccba
horaciocoronel <arielc@gmail.com>
2017-12-05 16:21:03 -0500

    Add validation that requires the presence of at least one of the fields price_above, price_below, and percent when creating a new alert

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,12 @@
 class Alert < ApplicationRecord
   belongs_to :following
+
+  validates :validate_at_least_one_field
+
+  private
+    def validate_at_least_one_field
+      unless !price_above.blank? or !price_below.blank? or !percent.blank?
+         record.errors[:base] << "Can't be blank"
+      end
+    end
 end
