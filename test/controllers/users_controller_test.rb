55cbbee024c50dc32adf02b72a99671019bbcc01
horaciocoronel <arielc@gmail.com>
2017-12-05 13:12:17 -0500

    Add factory-bot-rails and rspecs-rails gems

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,10 +2,17 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  empty_user = User.new
+  user = User.create(username: 'Test User', email: 'test@gmail.com', password: '123', password_confirmation: '123', phone: '1234567890', phone_alert: true, email_alert: true, avatar_url: '')
+
   test 'User cant be saved when empty' do
+    assert_not(empty_user.save, 'User saved empty')
+  end
 
-    user = User.new
+  test 'User logged in can follow a coin' do
 
-    assert_not(user.save, 'User saved empty')
+    coin_symbol = 'LTC'
+    following = Following.new(user_id: user, coin_name: coin_symbol)
+    assert_equal(following.save, true)
   end
 end
