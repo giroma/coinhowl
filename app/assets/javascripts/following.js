0c015ad17297ef2f09d21c3c680470b071ddc238
Michelle Cinello <m.cinello@gmail.com>
2017-12-05 08:18:10 -0500

    alerts button is showing up again once you follow a coin

--- app/assets/javascripts/following.js
+++ app/assets/javascripts/following.js
@@ -7,45 +7,44 @@ document.addEventListener('DOMContentLoaded', function() {
   var followLink = document.getElementById('follow-coin');
   var unfollowLink = document.getElementById('unfollow-coin');
   // We should remove the next line, the button won't be in the page untill the follow button is pressed, so defining it now will result in undefined
-  var alertIcon = document.getElementById('alert');
+  var alertIcon = document.getElementById('alert-icon');
   var alertForm = document.querySelector('.alert-form')
 
   // Add if statement to avoid getting error in all other pages that do not have follow/unfollow Link
   if (followLink != undefined) {
-  followLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    $.ajax({
-       url: e.target.getAttribute('href'),
-       method: e.target.dataset.method,
-       dataType: 'json'
-     }).done(function(data) {
-       // Define alert icon inside the function, once the button has been pressed, otherwise it doesn't have it defined yet.
-       var alertIcon = document.getElementById('alert');
-       followLink.classList.add('hide');
-       unfollowLink.classList.remove('hide');
-       alertIcon.classList.remove('hide');
-       var array = this.url.split("/");
-       var followingCode = this.url.split("/")[array.length-1]
-       populate_alert_form(followingCode);
-     }).fail(function(data) {
-     });
-  });
+    followLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      $.ajax({
+         url: e.target.getAttribute('href'),
+         method: e.target.dataset.method,
+         dataType: 'json'
+       }).done(function(data) {
+         // Define alert icon inside the function, once the button has been pressed, otherwise it doesn't have it defined yet.
+         followLink.classList.add('hide');
+         unfollowLink.classList.remove('hide');
+         alertIcon.classList.remove('hide');
+         var array = this.url.split("/");
+         var followingCode = this.url.split("/")[array.length-1]
+         populate_alert_form(followingCode);
+       }).fail(function(data) {
+       });
+    });
 }
   // Add if statement to avoid getting error in all other pages that do not have follow/unfollow Link
   if (unfollowLink != undefined) {
-  unfollowLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    $.ajax({
-       url: e.target.getAttribute('href'),
-       method: e.target.dataset.method,
-       dataType: 'json'
-     }).done(function(data) {
-       unfollowLink.classList.add('hide');
-       followLink.classList.remove('hide');
-       alertIcon.classList.add('hide');
-     }).fail(function(data) {
-     });
-  });
+    unfollowLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      $.ajax({
+         url: e.target.getAttribute('href'),
+         method: e.target.dataset.method,
+         dataType: 'json'
+       }).done(function(data) {
+         unfollowLink.classList.add('hide');
+         followLink.classList.remove('hide');
+         alertIcon.classList.add('hide');
+       }).fail(function(data) {
+       });
+    });
 }
   function populate_alert_form(followingCode) {
     $.ajax ({
@@ -56,4 +55,5 @@ document.addEventListener('DOMContentLoaded', function() {
       alertForm.innerHTML = html;
     });
   };
+
 });
