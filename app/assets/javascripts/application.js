ee023cf9adfe39e7e4468ae08187652346a250a7
Michelle Cinello <m.cinello@gmail.com>
2017-12-06 10:37:27 -0500

    fixed indentations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,33 +23,33 @@ $(document).ready(function() {
   $('.carousel.carousel-slider').carousel({fullWidth: true});
   // $('.carousel-slider').slider({full_width: true});//slider init
 
-
   var symbolHash = {}
   var myResults = function() {
     var url = "https://cors-anywhere.herokuapp.com/https://bittrex.com/api/v1.1/public/getmarkets";
-    $.ajax({
-    url: url,
-    method: 'GET',
-    dataType: 'JSON'
-    }).done(function(data){
-        data["result"].forEach(function(element) {
-          var tempSymbol = element["MarketName"].includes('BTC-','');
-            if(tempSymbol == true) {
-              if (element["MarketName"] == 'BTC-BCC') {
-                symbolHash['BCH Bitcoin Cash'] = "https://www.cryptocompare.com/media/1383919/bch.jpg"
-              } else {
-              var btcRemoved = element["MarketName"].replace('BTC-','');
-              symbolHash[btcRemoved+' '+element["MarketCurrencyLong"]] = element["LogoUrl"]
-              }
-            }
-        });
-        // console.log(symbolHash);
 
-        $('input.autocomplete').autocomplete({
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'JSON'
+    })
+    .done(function(data) {
+      data["result"].forEach(function(element) {
+        var tempSymbol = element["MarketName"].includes('BTC-','');
+        if (tempSymbol == true) {
+          if (element["MarketName"] == 'BTC-BCC') {
+            symbolHash['BCH Bitcoin Cash'] = "https://www.cryptocompare.com/media/1383919/bch.jpg"
+          } else {
+            var btcRemoved = element["MarketName"].replace('BTC-','');
+            symbolHash[btcRemoved+' '+element["MarketCurrencyLong"]] = element["LogoUrl"]
+          }
+        }
+      }
+    });
+
+      $('input.autocomplete').autocomplete({
          data: symbolHash,
          limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
          onAutocomplete: function(val) {
-           // console.log(val);
            // Callback function when value is autcompleted.
                // select: function( event, ui ) {
                  // var url = ui.item.label;
@@ -57,14 +57,12 @@ $(document).ready(function() {
                // },
          },
          minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-        });
+      });
 
-
-        return symbolHash;
-    });
+      return symbolHash;
   };
-  myResults();
 
+  myResults();
 
   // User logged in dropdown in navbar
   $(".dropdown-button").dropdown(
@@ -83,4 +81,5 @@ $(document).ready(function() {
   if ( toastAlert && toastAlert.innerText != null) {
     Materialize.toast(toastAlert, 4000);
   }
+
 });
