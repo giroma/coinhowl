409b03edda2f8e94a5fa73f50fda1013a2eacb89
Michelle Cinello <m.cinello@gmail.com>
2017-12-06 11:31:25 -0500

    fixed indentation in models

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -4,9 +4,9 @@ class Alert < ApplicationRecord
 
   validate :validate_at_least_one_field
 
-    def validate_at_least_one_field
-      if price_above.blank? && price_below.blank? && percent.blank?
-        errors.add :base, "Can't be blank!"
-      end
+  def validate_at_least_one_field
+    if price_above.blank? && price_below.blank? && percent.blank?
+      errors.add :base, "Can't be blank!"
     end
+  end
 end
