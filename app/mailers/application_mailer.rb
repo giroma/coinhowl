07cdad45d1835aa0d9a952ac2112a812dc08458c
Argir Argirov <dj.giro@gmail.com>
2017-12-07 16:13:37 -0500

    getting alerts to work in production and styling

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'support@coinhowl.com'
+  default from: 'coinhowl@gmail.com'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,14 +6,14 @@ class UserMailer < ApplicationMailer
   end
   def alert_email_above(email, alert)
     @url  = 'www.coinhowl.com'
-    mail(to: email, subject: "id #{alert.id} #{alert.following.coin_name} > #{alert.price_above}")
+    mail(to: email, subject: "#{alert.following.coin_name} > #{alert.price_above}")
   end
   def alert_email_below(email, alert)
     @url  = 'www.coinhowl.com'
-    mail(to: email, subject: "id #{alert.id} #{alert.following.coin_name} < #{alert.price_above}")
+    mail(to: email, subject: "#{alert.following.coin_name} < #{alert.price_above}")
   end
   def alert_email_percent(email, alert)
     @url  = 'www.coinhowl.com'
-    mail(to: email, subject: "id #{alert.id} #{alert.following.coin_name} price has changed over #{alert.percent}%")
+    mail(to: email, subject: "#{alert.following.coin_name} price has changed over #{alert.percent}%")
   end
 end
